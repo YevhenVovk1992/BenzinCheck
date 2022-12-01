293e2d41866794d34fa93c3787a05fbcaa2bb334
YevhenVovk <vovk.valki@ukr.net>
2022-12-01 16:58:42 +0200

    Create DELETE method for PriceAPIView

--- api_v10/views.py
+++ api_v10/views.py
@@ -10,6 +10,14 @@ from api_v10 import serializer
 
 # Create your views here.
 class PriceAPIView(APIView):
+    def get_params(self, **kwargs):
+        region = kwargs.get('region', None)
+        fuel = kwargs.get('fuel', None)
+        fuel_operator = kwargs.get('fuel_operator', None)
+        if not region or not fuel or not fuel_operator:
+            return Response({'error': 'Update method not allowed'})
+        return {'region': region, 'fuel': fuel, 'fuel_operator': fuel_operator}
+
     def get(self, request):
         date_now = timezone.now().date()
         obj = models.PriceTable.objects.filter(date=date_now).all()
@@ -26,16 +34,12 @@ class PriceAPIView(APIView):
 
     def put(self, request, **kwargs):
         date_now = timezone.now().date()
-        region = kwargs.get('region', None)
-        fuel = kwargs.get('fuel', None)
-        fuel_operator = kwargs.get('fuel_operator', None)
-        if not region or not fuel or not fuel_operator:
-            return Response({'error': 'Update method not allowed'})
+        params = self.get_params(**kwargs)
         try:
             instance = models.PriceTable.objects.filter(
-                id_fuel=models.Fuel.objects.filter(name=fuel).first(),
-                id_region=models.Region.objects.filter(name=region).first(),
-                id_fuel_operator=models.FuelOperator.objects.filter(name=fuel_operator).first(),
+                id_fuel=models.Fuel.objects.filter(name=params['fuel']).first(),
+                id_region=models.Region.objects.filter(name=params['region']).first(),
+                id_fuel_operator=models.FuelOperator.objects.filter(name=params['fuel_operator']).first(),
                 date=date_now
             ).first()
         except Exception as error:
@@ -44,3 +48,18 @@ class PriceAPIView(APIView):
         if model_serializer.is_valid():
             model_serializer.save(date=date_now)
             return Response({'Price_post': model_serializer.data})
+
+    def delete(self, request, **kwargs):
+        params = self.get_params(**kwargs)
+        try:
+            instance = models.PriceTable.objects.filter(
+                id_fuel=models.Fuel.objects.filter(name=params['fuel']).first(),
+                id_region=models.Region.objects.filter(name=params['region']).first(),
+                id_fuel_operator=models.FuelOperator.objects.filter(name=params['fuel_operator']).first(),
+                date=timezone.now().date()
+            ).first()
+        except Exception as error:
+            return Response({'error': error})
+        id_instance = instance.id
+        instance.delete()
+        return Response({'Price_delete': id_instance })
